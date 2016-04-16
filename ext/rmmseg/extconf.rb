a548382e7a8798babea222f0841bc7cfa8de982e
guochunzhong <guochunzhong@bayekeji.com>
2016-04-17 01:04:04 +0800

    Add windows support

--- ext/rmmseg/extconf.rb
+++ ext/rmmseg/extconf.rb
@@ -5,4 +5,9 @@ try_link('memory.cpp')
 try_link('dict.cpp')
 try_link('rmmseg.cpp')
 
+case RUBY_PLATFORM
+when /mswin32/, /mingw32/, /bccwin32/
+  CONFIG['LDSHAREDXX'] = "$(CXX) -shared -static-libgcc -static-libstdc++"
+end
+
 create_makefile('rmmseg')
